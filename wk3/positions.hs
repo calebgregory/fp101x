9501abb25bfb076557bcd843ac2658222716752d
calebgregory <calebgregory@gmail.com>
2015-10-29 23:00:04 -0500

    Adding fn to find values for keys in a collection and a 'position' fn to show the positions at which an element occurs within a collection

---
+++ wk3/positions.hs
@@ -0,0 +1,12 @@
+-- find :: (Eq a) => a -> [(a,b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
+
+-- positions :: (Eq a) => a -> [a] -> [Int]
+positions x xs =
+  find x (zip xs [0..n])
+  where n = length xs - 1
+
+
+-- should refurn ["a","c","d"]
+test = find 1 [(1,"a"),(0,"b"),(1,"c"),(1,"d"),(0,"e")]
+test1 = positions 1 [1,0,1,1,0,1]
